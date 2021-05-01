324a031b2cde4385cf2c53b358a72b5297ebdc56
ggarrett13 <g.h.garrett13@gmail.com>
2021-05-01 20:58:46 +0200

    bumped version to 0.0.7

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,7 @@ with open("../AUTHORS", "r") as f:
     author = ', '.join(list(re.findall(r"^\*\s(.*)", f.read(), re.MULTILINE)))
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release='0.0.7'
 
 # -- General configuration ---------------------------------------------------
 
