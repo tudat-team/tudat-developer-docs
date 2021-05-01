a1c7e30054f26e919686162307861f6e6d731049
ggarrett13 <g.h.garrett13@gmail.com>
2021-05-01 20:23:00 +0200

    bumped version to 0.0.1

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ copyright = '2021, Tudat Space'
 author = 'Tudat Space'
 
 # The full version, including alpha/beta/rc tags
-release='author'
+release='0.0.1'
 
 # -- General configuration ---------------------------------------------------
 
